703d96a50e40d981aac0182449925e7a2ce43ddf
hache0099 <hache0099_github@protonmail.com>
2025-04-04 19:31:15 -0300

    Modificado App.vue para prueba

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,21 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div id="app">
+    <div class="container mt-5">
+      <!-- Barra de navegación opcional -->
+      <nav>
+        <router-link to="/">Home</router-link> |
+        <router-link to="/login">Login</router-link>
+      </nav>
+
+      <!-- Aquí se cargarán las páginas -->
+      <router-view/>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
-  
 </template>
 
-<style scoped>
+<!--style scoped>
 .logo {
   height: 6em;
   padding: 1.5em;
@@ -28,4 +30,4 @@ import HelloWorld from './components/HelloWorld.vue'
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
-</style>
+</style-->
