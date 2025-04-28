4e4bf57577af3a8fb3b10e7000e5970df8ed2740
hache0099 <hache0099_github@protonmail.com>
2025-04-28 17:14:24 -0300

    Creado componente para manejar el logout

---
+++ src/components/Logout.vue
@@ -0,0 +1,17 @@
+<script>
+import authService from '../services/auth.service'; 
+
+export default {
+  methods: {
+    async logout() {
+      const response = await authService.logout();
+      
+      this.$router.push('/');
+    }
+  },
+  created() {
+    this.logout();
+  }
+}
+</script>
+<template></template>
