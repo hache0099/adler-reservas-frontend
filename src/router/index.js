7e5cf92b1805764563d87aea70a3841a9610deb6
hache0099 <hache0099_github@protonmail.com>
2025-04-28 17:15:35 -0300

    Agregadas nuevas rutas

--- src/router/index.js
+++ src/router/index.js
@@ -3,9 +3,16 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 // Importa tus componentes (páginas)
 import Login from '../components/Login.vue';
+import Logout from '../components/Logout.vue';
 import Home from '../components/Home.vue'; 
 import Register from '../components/Register.vue';
 import Profile from '../components/Profile.vue';
+import Reserva from '../components/Reserva.vue';
+import NuevaReserva from '../components/NuevaReserva.vue';
+import Gestion from '../components/gestion/Index.vue';
+import TablasTemplate from '../components/gestion/TablasTemplate.vue';
+import GestionCanchas from '../components/gestion/Cancha.vue';
+import GestionPerfiles from '../components/gestion/Perfil.vue';
 
 // Define las rutas
 const routes = [
@@ -13,28 +20,102 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home, // Página principal
+    meta: {hideSidebar: true},
   },
   {
     path: '/login',
     name: 'Login',
     component: Login, // Página de login
+    meta: {hideSidebar: true},
+  },
+  {
+    path: '/logout',
+    name: 'Logout',
+    component: Logout,
+    meta: {hideSidebar: true},
   },
   {
     path: '/register',
     name: 'Register',
     component: Register, // Página de registro
+    meta: {hideSidebar: true},
   },
   {
     path: '/profile',
     name: 'Perfil',
     component: Profile,
-  }
+    meta: {hideSidebar: false},
+  },
+  {
+    path: '/reservas',
+    name: 'Reservas',
+    component: Reserva,
+    meta: {hideSidebar: false},
+  },
+  {
+    path: '/reservas/nueva',
+    name: 'Nueva Reserva',
+    component: NuevaReserva,
+    meta: {hideSidebar: false},
+  },
+  {
+    path: '/gestion',
+    name: 'Gestion',
+    component: Gestion,
+    meta: {hideSidebar: false},
+  },
+  {
+    path: '/gestion/tipos-documento',
+    name: 'Gestion Tipos Documento',
+    component: TablasTemplate,
+    meta: {hideSidebar: false},
+  },
+  {
+    path: '/gestion/tipos-contacto',
+    name: 'Gestion Tipos Contacto',
+    component: TablasTemplate,
+    meta: {hideSidebar: false},
+  },
+  {
+    path: '/gestion/tipos-domicilio',
+    name: 'Gestion Tipos Domicilio',
+    component: TablasTemplate,
+    meta: {hideSidebar: false},
+  },
+  {
+    path: '/gestion/tipos-cancha',
+    name: 'Gestion Tipos Cancha',
+    component: TablasTemplate,
+    meta: {hideSidebar: false},
+  },
+  {
+    path: '/gestion/canchas',
+    name: 'Gestion Canchas',
+    component: GestionCanchas,
+    meta: {hideSidebar: false},
+  },
+  {
+    path: '/gestion/perfiles',
+    name: 'Gestion Perfiles',
+    component: GestionPerfiles,
+    meta: {hideSidebar: false},
+  },
 ];
 
 // Crea el router
 const router = createRouter({
   history: createWebHistory(),
   routes, // Rutas definidas
+});
+
+router.beforeEach(async (to, from) => {
+  const isAuthenticated = localStorage.getItem('userToken') !== null;
+  const noAuthReq = ['Register', 'Login', 'Home'];
+
+
+  if(!isAuthenticated && !noAuthReq.includes(to.name)){
+    return { name: 'Login' }
+  }
 });
 
 export default router;
